a89bc453dab74455faff0e871c1b561be733552f
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-15 01:12:09 +0800

    Project Euler problem number 010 solved

---
+++ Eulers Project/ex010_summation_of_primes/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace ex010_summation_of_primes.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("ex010_summation_of_primes")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("ex010_summation_of_primes")>]
+[<assembly: AssemblyCopyright("Copyright ©  2018")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("4ba19735-3a0b-4697-ba16-7ca1356fc67a")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Eulers Project/ex010_summation_of_primes/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let n : seq<int64> = seq {yield 2L; yield! Seq.initInfinite (fun i -> (int64 i * 2L) + 3L)} 
+                     |> Seq.takeWhile (fun i -> int64 i < 2_000_000L)
+
+let isPrime (x : int64) : bool = 
+    match x with
+    | _  when x = 2L -> true
+    | _ ->
+        let maxDiv = int64 (sqrt (float x)) 
+        let rec f (d : int64) : bool = 
+            if d > maxDiv then true
+            else if x % d = 0L then false
+            else f (d + 2L)
+        f 3L
+
+let primesSummed = Seq.filter (isPrime) n |> Seq.sum
+
+printfn ("All prime numbers under 2 million summed : %i") <| int primesSummed
+
+Console.ReadKey () |> ignore
