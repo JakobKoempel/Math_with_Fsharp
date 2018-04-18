1176d85fa70f43f9e9a6c7961b8bb09d148efdf3
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-18 19:42:14 +0800

    simplified

--- Eulers Project/ex006_sum_square_difference/Program.fs
+++ Eulers Project/ex006_sum_square_difference/Program.fs
@@ -1,15 +1,5 @@
-﻿open System
+﻿
+let n = Seq.init 100 ((+) 1)
 
-let limit = 100
-let n (i : int) = Seq.init i (fun j -> j + 1)
-
-let square_add (s : seq<int>) : int =
-    let squared = Seq.map (fun i -> i * i) s
-    Seq.sum squared
-
-let add_square (s : seq<int>) =
-       int (float (Seq.sum s) ** 2.)
-
-let dif = add_square (n limit) - square_add (n limit)
-
-printfn("%i") <| dif 
+(n |> Seq.fold (fun acc i -> acc + i * i) 0) - pown (n |> Seq.sum) 2 |> abs
+|> printfn "%i"
