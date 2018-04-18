07d62bcb46574b918e48e5d55d76ab68a62dfc9f
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-19 01:07:03 +0800

    solution for the problem number 008

---
+++ Eulers Project/ex008_largest_product_in_a_series/Program.fs
@@ -0,0 +1,10 @@
+﻿open System.IO
+
+File.ReadLines "Series.txt" 
+|> Seq.concat 
+|> Seq.map (fun i -> int64 i - int64 '0')
+|> Seq.windowed 13  
+|> Seq.map (Array.reduce (*))
+|> Seq.max 
+|> printfn("%i")
+
