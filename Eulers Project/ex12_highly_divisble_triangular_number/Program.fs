4e188c858f400c77f2cf08623893d6f730fb404d
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-20 18:08:08 +0800

    readable code

--- Eulers Project/ex12_highly_divisble_triangular_number/Program.fs
+++ Eulers Project/ex12_highly_divisble_triangular_number/Program.fs
@@ -1,20 +1,14 @@
-﻿open System
+﻿let triangularNum (n : int) : int = (n * (n - 1)) / 2 
 
-let triangularNum (n : int64) = (n * (n + 1L)) / 2L
-
-let divisors (n : int64) : int64 = 
-    let rec loop (i : int64) (div : int64) : int64 = 
+let countDivisors (n : int) : int = 
+    let rec loop (i : int) (div : int) : int = 
         match i * i with
-        | i2 when i2 < n -> if n % i = 0L then loop (i + 1L) (div + 2L)
-                            else loop (i + 1L) div
+        | i2 when i2 < n -> if n % i = 0 then loop (i + 1) (div + 2)
+                            else loop (i + 1) div
         | i2 when i2 > n -> div
-        | _ -> div + 1L
-    loop 1L 0L
+        | _ -> div + 1
+    loop 1 0
 
-let rec loop (i : int64) : int64 =
-    if triangularNum i |> divisors > 500L then triangularNum i
-    else loop (i + 1L)
-
-printfn("%i") <| loop 1L
-
-Console.ReadKey () |> ignore
+Seq.initInfinite (fun i -> triangularNum i) 
+|> Seq.find (fun n -> countDivisors n > 500) 
+|> printfn("%i")
