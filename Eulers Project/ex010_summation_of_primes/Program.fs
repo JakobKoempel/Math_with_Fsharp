4a42721f8658f4900e253a0e7c86836caea483ce
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-20 19:13:56 +0800

    typo fixed

--- Eulers Project/ex010_summation_of_primes/Program.fs
+++ Eulers Project/ex010_summation_of_primes/Program.fs
@@ -1,11 +1,9 @@
-﻿open System
-
-let n : seq<int64> = seq {yield 2L; yield! Seq.initInfinite (fun i -> (int64 i * 2L) + 3L)} 
+﻿let n : seq<int64> = seq {yield 2L; yield! Seq.initInfinite (fun i -> (int64 i * 2L) + 3L)} 
                      |> Seq.takeWhile (fun i -> int64 i < 2_000_000L)
 
 let isPrime (x : int64) : bool = 
     match x with
-    |_ when 2L = x -> true   
+    | _ when 2L = x -> true   
     | _ when 2L > x  || x % 2L = 0L -> false 
     | _ ->
         let maxDiv = int64 (sqrt (float x)) 
@@ -15,8 +13,6 @@ let isPrime (x : int64) : bool =
             else f (d + 2L)
         f 3L
 
-let primesSummed = Seq.filter (isPrime) n |> Seq.sum
-
-printfn ("All prime numbers under 2 million summed : %A") <| primesSummed 
-
-Console.ReadKey () |> ignore
+Seq.filter (isPrime) n 
+|> Seq.sum
+|> printfn ("All prime numbers under 2 million summed : %i") 
