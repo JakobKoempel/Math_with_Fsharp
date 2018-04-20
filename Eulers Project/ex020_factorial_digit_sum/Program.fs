1d3f8dad7ba7b29dcaae13a91869a8c83aaad920
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-21 06:06:54 +0800

    solution to the 20th problem

---
+++ Eulers Project/ex020_factorial_digit_sum/Program.fs
@@ -0,0 +1,7 @@
+﻿let rec factorial (n : bigint) : bigint =
+        if n = 1I then 1I
+        else n * factorial (n - 1I) 
+
+string (factorial 100I)
+|> Seq.fold (fun acc i -> acc + int i - int '0') 0
+|> printfn("%i")
