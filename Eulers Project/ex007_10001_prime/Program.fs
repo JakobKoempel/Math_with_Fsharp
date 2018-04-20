f8f67b2a7646c9f87ef7fb02f9b7b7bfb2eb25a1
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-20 23:00:15 +0800

    cleaned code

--- Eulers Project/ex007_10001_prime/Program.fs
+++ Eulers Project/ex007_10001_prime/Program.fs
@@ -1,12 +1,9 @@
-﻿open System
-
-let (x : int) = 10001
-let n = seq {yield 2; yield! Seq.initInfinite (fun i -> i * 2 + 3) }
+﻿let n = seq {yield 2; yield! Seq.initInfinite (fun i -> i * 2 + 3) }
 
 let isPrime (x : int) : bool = //only natural numbers
     match x with
     | _ when 2 = x -> true   
-    | _ when 2 > x  || x % 2 = 0 -> false 
+    //| _ when 2 > x  || x % 2 = 0 -> false 
     | _ ->
         let maxDiv = int (sqrt (float x)) 
         let rec f (d : int) : bool = 
@@ -15,15 +12,6 @@ let isPrime (x : int) : bool = //only natural numbers
             else f (d + 2)
         f 3
 
-let findXthPrime (x : int) : int =
-    let primes = Seq.filter isPrime n
-    let rec f (p : seq<int>) (i : int) : int =
-        if x = i then Seq.head p
-        else f (Seq.tail p) (i + 1)
-    f primes 1
-
-printfn("%i is the %ist prime number") (findXthPrime x) x
-
-Console.ReadKey() |> ignore
-
-
+Seq.filter isPrime n
+|> Seq.item 10001
+|> printfn("%i")
--- Eulers Project/ex16_power_digit_sum/Program.fs
+++ Eulers Project/ex16_power_digit_sum/Program.fs
@@ -1,3 +1,3 @@
-﻿string (1I <<< 1000)
+﻿string (1I <<< 1000) 
 |> Seq.fold (fun acc i -> acc + int i - int '0') 0
 |> printfn("%i") 
