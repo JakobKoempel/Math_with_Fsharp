620d35e5c757b82939816719c242884ed476f74c
Jakob Kömpel <jakob.koempel@gmail.com>
2018-04-22 09:08:11 +0800

    solution for the 25th problem

---
+++ Eulers Project/ex025_1000_digit_fibonacci_number/Program.fs
@@ -0,0 +1,9 @@
+﻿let fibonacci : bigint seq =
+    let rec loop a b = seq {yield a ; yield! loop b (a + b)}
+    Seq.append [0I] (loop 1I 1I)
+
+
+
+fibonacci
+|> Seq.findIndex (fun i -> Seq.length (string i) = 1000)
+|> printfn("%A")
