2f64c8b58855d992f376993cafd45a49c3fe5eb2
Jakob Kömpel <jakob.koempel@gmail.com>
2018-05-02 00:47:41 +0200

    solution for project 013

---
+++ Eulers Project/ex013_large_sum/Program.fs
@@ -0,0 +1,11 @@
+﻿open System.IO
+open System.Numerics
+
+let firstTenDigitsOfSum = File.ReadAllLines "Series.txt" 
+                        |> Seq.sumBy (fun i -> BigInteger.Parse i) 
+                        |> string
+                        |> Seq.take 10 
+
+let digitsToInt (digits : char seq) = new string [|for i in digits -> i|]
+                                    |> int64
+printfn("%i") <| digitsToInt  firstTenDigitsOfSum
