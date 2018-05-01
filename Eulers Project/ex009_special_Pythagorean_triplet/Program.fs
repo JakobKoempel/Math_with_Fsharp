96746918e2871aa466df0822fa9797f1c8e97e23
Jakob Kömpel <jakob.koempel@gmail.com>
2018-05-01 13:48:16 +0200

    simplified code

--- Eulers Project/ex009_special_Pythagorean_triplet/Program.fs
+++ Eulers Project/ex009_special_Pythagorean_triplet/Program.fs
@@ -1,24 +1,17 @@
-﻿type Triplet = {
-    a : int
-    b : int
-    c : int
-}
+﻿let isWholeNumber (x : float) : bool = 
+    x % 1.0 = 0.0
 
-let isWholeNumber (x : float) : bool = 
-    if x % 1.0 = 0.0 then true
-    else false
-
-let findTripletWithCircumferenceOf (limit : int) : Triplet =
+let findTripletWithCircumferenceOf (target : int) : int * int * int =
     let hypotenuse (a : int, b : int) : float = sqrt(float(a * a + b * b))  
-    let rec f (a : int, b : int) : Triplet =
+    let rec f (a : int, b : int) : int * int * int =
         let c = hypotenuse (a, b)
         if isWholeNumber c then 
             match a + b + int c with
-                | Cf when Cf < limit -> f (a, b + 1)
-                | Cf when Cf > limit -> f (a + 1, a + 2)
-                | _ -> {a = a ; b = b ; c = int c}
+                | circumference when circumference < target -> f (a, b + 1)
+                | circumference when circumference > target -> f (a + 1, a + 2)
+                | _ -> (a, b, int c)
         else f (a, b + 1) 
     f (1, 2)
 
-let resultTriplet =  findTripletWithCircumferenceOf 1000       
-printfn("%i") <| (resultTriplet.a * resultTriplet.b * resultTriplet.c)
+let (a : int, b : int, c : int) =  findTripletWithCircumferenceOf 1000       
+printfn("%i") <| (a * b * c)
