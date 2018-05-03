de615ca69a273a54882394c42c51cc5cc3f10e72
Jakob Kömpel <jakob.koempel@gmail.com>
2018-05-03 13:35:09 +0200

    test

---
+++ Basic calculus/integral_basic/Program.fs
@@ -0,0 +1,10 @@
+﻿let g (x : float) = x ** 3. * 2. + 5.
+
+let integral (f : float -> float) (a : float) (b : float) (stepwidth : float) : float =
+    let rec loop (x : float) (acc : float) : float =
+        if x < b then
+            loop (x + stepwidth) (acc + f x * stepwidth) 
+        else acc
+    loop a 0.
+
+printfn("%f") <| integral g 0. 1. 0.000001
