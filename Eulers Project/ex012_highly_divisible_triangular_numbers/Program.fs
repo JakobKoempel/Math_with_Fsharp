b98b5b3dc14d464e57210b5187838293c15ae6ad
Jakob Kömpel <jakob.koempel@gmail.com>
2018-05-03 08:16:18 +0200

    renamed variable

--- Eulers Project/ex012_highly_divisible_triangular_numbers/Program.fs
+++ Eulers Project/ex012_highly_divisible_triangular_numbers/Program.fs
@@ -1,12 +1,12 @@
 ﻿let triangularNum (n : int) : int = (n * (n - 1)) / 2 
 
 let countDivisors (n : int) : int = 
-    let rec loop (i : int) (div : int) : int = 
+    let rec loop (i : int) (divCount : int) : int = 
         match i * i with
-        | i2 when i2 < n -> if n % i = 0 then loop (i + 1) (div + 2)
-                            else loop (i + 1) div
-        | i2 when i2 > n -> div
-        | _ -> div + 1
+        | i2 when i2 < n -> if n % i = 0 then loop (i + 1) (divCount + 2)
+                            else loop (i + 1) divCount
+        | i2 when i2 > n -> divCount
+        | _ -> divCount + 1
     loop 1 0
 
 Seq.initInfinite (fun i -> triangularNum i) 
